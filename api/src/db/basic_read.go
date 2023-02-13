9724f3d538599a5a5e101e7a2340727157dbf371
Kiskutnah <alexmclifton@gmail.com>
2023-02-13 22:34:44 +0000

    chhecking access keys

--- api/src/db/basic_read.go
+++ api/src/db/basic_read.go
@@ -6,11 +6,6 @@ import (
 	"gorm.io/gorm"
 )
 
-// // TableName overrides the table name used by User to `profiles`
-// func (testTable) TableName() string {
-// 	return "testTable"
-// }
-
 func GetGames(conn gorm.DB) []HumbleGame {
 	games := []HumbleGame{}
 	conn.Find(&games)
@@ -18,4 +13,21 @@ func GetGames(conn gorm.DB) []HumbleGame {
 	fmt.Println("games:", games)
 
 	return games
+}
+
+func CheckAccessKey(generatedKey string, conn gorm.DB) bool {
+	testForUnique := AccessKey{}
+	conn.Where(&AccessKey{Key: generatedKey}).First(&testForUnique)
+	// if there's no ID (as in ID == 0), we know it's unique
+	// tbh, what are teh chances they aren't unique, but may aswell check
+	if testForUnique.ID != 0 {
+		return true
+	}
+	return false
+}
+
+func GetGamekey(gameID int, conn gorm.DB) HumbleGame {
+	gameData := HumbleGame{}
+	conn.Where(&HumbleGame{ID: uint(gameID)}).First(&gameData)
+	return gameData
 }
